fc95512ee8a5c67069c3151232559f97703669f6
larrykkk <xlarry1000@gmail.com>
2018-11-07 19:30:15 +0800

    update

--- models/db.js
+++ models/db.js
@@ -1,11 +1,19 @@
 var mysql = require("mysql");
 var db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "nodeproject",
-  port: "3306"
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  user: "ba3d4254d4f42e",
+  password: "28dc64c9",
+  database: "heroku_d8b573c814ae11b"
+  // port: "3306"
 });
+
+// var db = mysql.createConnection({
+//   host: "us-cdbr-iron-east-01.cleardb.net",
+//   user: "ba3d4254d4f42e",
+//   password: "28dc64c9",
+//   database: "heroku_d8b573c814ae11b"
+//   // port: "3306"
+// });
 
 db.connect(function(err) {
   if (err) throw err;
