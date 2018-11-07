c5c6688ca65fccafa0130aee6c5fc8fdd341e2ab
larrykkk <xlarry1000@gmail.com>
2018-11-07 20:39:20 +0800

    add hold db

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 
-var holdDB = require("./middlewares/query");
-
 //passport
 var session = require("express-session");
 const passport = require("passport");
@@ -88,7 +86,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(holdDB);
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -98,5 +96,41 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+var db_config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "example"
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  connection.connect(function(err) {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on("error", function(err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = app;
