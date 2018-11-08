80c8f7d46c4c8331cf00d99d2b817e5044d7d4a7
larrykkk <xlarry1000@gmail.com>
2018-11-08 16:41:15 +0800

    修改路徑 前台導向後台

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // /* GET home page. */
-// router.get("/", function(req, res, next) {
-//   res.render("practice", { title: "Express" });
-// });
+router.get("/", function(req, res, next) {
+  res.redirect("http://localhost:3000/console/login");
+});
 
 module.exports = router;
