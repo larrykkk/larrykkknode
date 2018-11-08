1cf39955f40fdd4417184eda7347d73c6f6122d3
larrykkk <xlarry1000@gmail.com>
2018-11-08 09:11:58 +0800

    fix

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -6,7 +6,7 @@ function isLoggedIn(req, res, next) {
   // if they aren't redirect them to the home page
   console.log("oo");
   // res.redirect("http://localhost:3000/console/login");
-  res.redirect("https://larrykkknode.herokuapp.com/console/login");
+  res.redirect("./login");
 }
 
 module.exports = isLoggedIn;
